3f38019f0276fd3854c74f54f1ab20f20aeed41a
Martin Raiber <martin@urbackup.org>
2011-10-31 16:52:53 +0100

    Seperate action id for transfer resume

--- urbackup/fileclient/packet_ids.h
+++ urbackup/fileclient/packet_ids.h
@@ -4,6 +4,7 @@
 typedef unsigned char uchar;
 
 const uchar ID_GET_FILE=0;
+const uchar ID_GET_FILE_RESUME=6;
 	const uchar ID_COULDNT_OPEN=0;
 	const uchar ID_FILESIZE=1;
 	const uchar ID_BASE_DIR_LOST=2;
