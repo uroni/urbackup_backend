8b00f10d3ce837b228dbe5176776c1517bc0a412
Martin Raiber <martin@urbackup.org>
2011-12-17 17:26:40 +0100

    Optimized incremental image sending

--- urbackup/ClientService.cpp
+++ urbackup/ClientService.cpp
@@ -2063,6 +2063,7 @@ void ClientConnector::sendFullImageThread(void)
 			unsigned int blocksize=(unsigned int)fs->getBlocksize();
 			int64 drivesize=fs->getSize();
 			int64 blockcnt=fs->calculateUsedSpace()/blocksize;
+			int64 ncurrblocks=0;
 
 			if(startpos==0)
 			{
@@ -2134,6 +2135,8 @@ void ClientConnector::sendFullImageThread(void)
 					break;
 				}
 
+				ncurrblocks+=secs.size();
+				
 				if(IdleCheckerThread::getPause())
 				{
 					Server->wait(5000);
@@ -2201,11 +2204,15 @@ void ClientConnector::sendIncrImageThread(void)
 {
 	char *blockbuf=NULL;
 	char *blockdatabuf=NULL;
+	bool *has_blocks=NULL;
+	char *zeroblockbuf=NULL;
 
 	bool has_error=true;
 
 	int save_id=-1;
 	int update_cnt=0;
+
+	unsigned int lastsendtime=Server->getTimeMS();
 
 	bool run=true;
 	while(run)
@@ -2291,9 +2298,17 @@ void ClientConnector::sendIncrImageThread(void)
 				}
 			}
 
+			delete []blockbuf;
 			blockbuf=new char[vhdblocks*blocksize];
+			delete []has_blocks;
+			has_blocks=new bool[vhdblocks];
+			delete []zeroblockbuf;
+			zeroblockbuf=new char[blocksize];
+			memset(zeroblockbuf, 0, blocksize);
+
 			ClientSend *cs=new ClientSend(pipe, blocksize+sizeof(int64), 2000);
 			THREADPOOL_TICKET send_ticket=Server->getThreadPool()->execute(cs);
+
 
 			for(int64 i=startpos,blocks=drivesize/blocksize;i<blocks;i+=vhdblocks)
 			{
@@ -2318,6 +2333,7 @@ void ClientConnector::sendIncrImageThread(void)
 				if(has_data)
 				{
 					sha256_init(&shactx);
+					bool mixed=false;
 					for(int64 j=i;j<blocks && j<i+vhdblocks;++j)
 					{
 						char *dpos=blockbuf+(j-i)*blocksize;
@@ -2328,11 +2344,13 @@ void ClientConnector::sendIncrImageThread(void)
 								Server->wait(5000);
 							}
 							sha256_update(&shactx, (unsigned char*)dpos, blocksize);
+							has_blocks[j-i]=true;
 						}
 						else
 						{
-							memset(dpos, 0, blocksize);
-							sha256_update(&shactx, (unsigned char*)dpos, blocksize);
+							sha256_update(&shactx, (unsigned char*)zeroblockbuf, blocksize);
+							has_blocks[j-i]=false;
+							mixed=true;
 						}
 					}
 					unsigned char digest[c_hashsize];
@@ -2347,28 +2365,40 @@ void ClientConnector::sendIncrImageThread(void)
 						}
 						if(memcmp(hashdata_buf, digest, c_hashsize)!=0)
 						{
+							//Server->Log("Block did change: "+nconvert(i)+" mixed="+nconvert(mixed), LL_DEBUG);
 							for(int64 j=i;j<blocks && j<i+vhdblocks;++j)
 							{
-								char* cb=cs->getBuffer();
-								memcpy(&cb[sizeof(int64)], blockbuf+(j-i)*blocksize, blocksize);
-								memcpy(cb, &j, sizeof(int64) );
-								cs->sendBuffer(cb, sizeof(int64)+blocksize);
-
-								if(cs->hasError())
+								if(has_blocks[j-i])
 								{
-									Server->Log("Pipe broken -2", LL_ERROR);
-									run=false;
-									break;
+									char* cb=cs->getBuffer();
+									memcpy(&cb[sizeof(int64)], blockbuf+(j-i)*blocksize, blocksize);
+									memcpy(cb, &j, sizeof(int64) );
+									cs->sendBuffer(cb, sizeof(int64)+blocksize);
+
+									lastsendtime=Server->getTimeMS();
+
+									if(cs->hasError())
+									{
+										Server->Log("Pipe broken -2", LL_ERROR);
+										run=false;
+										break;
+									}
 								}
 							}							
 						}
 						else
 						{
 							//Server->Log("Block didn't change: "+nconvert(i), LL_DEBUG);
-							int64 bs=-125;
-							char* buffer=cs->getBuffer();
-							memcpy(buffer, &bs, sizeof(int64) );
-							cs->sendBuffer(buffer, sizeof(int64));
+							unsigned int tt=Server->getTimeMS();
+							if(tt-lastsendtime>10000)
+							{
+								int64 bs=-125;
+								char* buffer=cs->getBuffer();
+								memcpy(buffer, &bs, sizeof(int64) );
+								cs->sendBuffer(buffer, sizeof(int64));
+
+								lastsendtime=tt;
+							}
 						}
 					}
 					else
@@ -2378,6 +2408,19 @@ void ClientConnector::sendIncrImageThread(void)
 						break;
 					}
 					if(!run)break;
+				}
+				else
+				{
+					unsigned int tt=Server->getTimeMS();
+					if(tt-lastsendtime>10000)
+					{
+						int64 bs=-125;
+						char* buffer=cs->getBuffer();
+						memcpy(buffer, &bs, sizeof(int64) );
+						cs->sendBuffer(buffer, sizeof(int64));
+
+						lastsendtime=tt;
+					}
 				}
 			}
 
@@ -2407,6 +2450,8 @@ void ClientConnector::sendIncrImageThread(void)
 	}
 
 	delete [] blockbuf;
+	delete [] has_blocks;
+	delete [] zeroblockbuf;
 
 	Server->destroy(hashdatafile);
 
