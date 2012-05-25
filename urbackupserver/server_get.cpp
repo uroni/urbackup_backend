6ac242278be222e0880f5f175771cb55fcdfb79a
Martin Raiber <martin@urbackup.org>
2012-05-25 20:03:46 +0200

    Send mails only to users with the right to receive them

--- urbackupserver/server_get.cpp
+++ urbackupserver/server_get.cpp
@@ -2463,7 +2463,7 @@ void BackupServerGet::sendLogdataMail(bool r_success, int image, int incremental
 			Tokenize(logr, toks, L",");
 			for(size_t j=0;j<toks.size();++j)
 			{
-				if(toks[j]==res_users[i][L"id"])
+				if(watoi(toks[j])==clientid)
 				{
 					has_r=true;
 				}
