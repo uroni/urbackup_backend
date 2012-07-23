591990c7c24f7f86c8fc81a556e06d798628d8f5
Martin Raiber <martin@urbackup.org>
2012-07-23 16:53:49 +0200

    Fixed deadlock in internet server

--- urbackupserver/InternetServiceConnector.cpp
+++ urbackupserver/InternetServiceConnector.cpp
@@ -308,9 +308,13 @@ void InternetServiceConnector::ReceivePackets(void)
 
 						is_connected=true;
 
-						IScopedLock lock(mutex);
-						client_data[clientname].last_seen=Server->getTimeMS();
 						
+						{
+							IScopedLock lock(mutex);
+							client_data[clientname].last_seen=Server->getTimeMS();
+						}
+						
+						IScopedLock lock(local_mutex);
 						connection_done_cond->notify_all();
 						connection_done_cond=NULL;
 					}
@@ -358,6 +362,8 @@ IPipe *InternetServiceConnector::getConnection(const std::string &clientname, ch
 
 			if(isc==NULL)
 				continue;
+				
+			lock.relock(NULL);
 
 			ICondition *cond_ok=Server->createCondition();
 			ICondition *cond_stop=Server->createCondition();
@@ -370,8 +376,8 @@ IPipe *InternetServiceConnector::getConnection(const std::string &clientname, ch
 					rtime=0;
 
 				if(rtime<100) rtime=100;
-
-				cond_ok->wait(&lock, rtime);
+				
+				isc->localWait(cond_ok, rtime);
 				
 				if(!isc->isConnected())
 				{
@@ -532,4 +538,10 @@ std::vector<std::string> InternetServiceConnector::getOnlineClients(void)
 bool InternetServiceConnector::hasTimeout(void)
 {
 	return has_timeout;
+}
+
+void InternetServiceConnector::localWait(ICondition *cond, int timems)
+{
+	IScopedLock lock(local_mutex);
+	cond->wait(&lock, timems);
 }
