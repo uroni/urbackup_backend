ac8240a01f938e6d649bbe2e8752b50148eaaefb
Martin <martin@urbackup.org>
2013-04-14 12:23:12 +0200

    Windows build on build server

--- build_server.bat
+++ build_server.bat
@@ -24,14 +24,26 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server" /p:Platform="win32"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+mkdir "Release Server"
+copy "urbackupserver\Release Server\*" "Release Server\"
+
 msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server" /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+mkdir "x64\Release Server"
+copy "urbackupserver\x64\Release Server\*" "x64\Release Server\"
 
 msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server PreVista" /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+mkdir "RRelease Server PreVista"
+copy "urbackupserver\Release Server PreVista\*" "Release Server PreVista\"
+
 msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server PreVista" /p:Platform="win32"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+mkdir "x64\Release Server PreVista"
+copy "urbackupserver\x64\Release Server PreVista\*" "x64\Release Server PreVista\"
 
 "%~dp0urbackupserver_installer_win/generate_msi.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
