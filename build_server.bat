875044e09976c8357b63a8134d223c1c6741f9fd
Martin <martin@urbackup.org>
2013-04-14 12:09:01 +0200

    Windows build on build server

--- build_server.bat
+++ build_server.bat
@@ -21,6 +21,18 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild UrBackupBackend.sln /p:Configuration="Release x64" /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server" /p:Platform="win32"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server" /p:Platform="x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server PreVista" /p:Platform="x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+msbuild urbackupserver\urbackupserver.vcxproj /p:Configuration="Release Server PreVista" /p:Platform="win32"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 "%~dp0urbackupserver_installer_win/generate_msi.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
