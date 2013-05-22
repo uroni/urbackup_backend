613f4ee060aa4558daf0192a7a537fc62276a87b
Martin Raiber <martin@urbackup.org>
2013-05-22 11:37:52 +0200

    Added build server script for debian

---
+++ build_server_debian.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+wget http://urpc.dyndns.org/urbackup-debian.tar.gz -O urbackup-debian.tar.gz
+tar xzf urbackup-debian.tar.gz
+
+if ! test -e build_server_debian_ok
+then
+	./switch_build.sh server
+	autoreconf || true
+	automake --add-missing || true
+	libtoolize || true
+	autoreconf
+	./configure --with-pychart --enable-packaging --enable-install_initd
+	touch build_server_debian_ok
+fi
+
+make dist > tmp
+BASENAME=`awk -F'"' '{ for(i=1; i<NF; i++) if($i ~/urbackup-server-(.*)/) {print $i; exit;} }' tmp`
+ANAME="${BASENAME}.tar.gz"
+VERSION=`echo $BASENAME | cut -d "-" -f 3`
+DEBVERSION="${VERSION}-1"
+
+sed -i "0,/urbackup-server \(.*\)(.*)/s//urbackup-server \($VERSION\)\1/" debian/changelog
+
+dh clean
+dh binary
