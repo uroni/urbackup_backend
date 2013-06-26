10a91e65a6b287984f06e677bb26a3642e9292aa
Martin Raiber <martin@urbackup.org>
2013-06-26 17:03:26 +0200

    Changed client branch

--- checkout_client.bat
+++ checkout_client.bat
@@ -2,5 +2,7 @@ if not exist client git clone -b next http://urpc.dyndns.org/git/urbackup_fronte
 
 cd client
 
+git fetch
+git checkout -b feature-quota origin/feature-quota
 git reset --hard
 git pull
