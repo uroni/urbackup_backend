5c77cab8dc7737bb89d56ff5e88adbd5b4e0b85f
Martin <martin@urbackup.org>
2013-07-29 22:59:11 +0200

    Fixed error message during boot

--- restore_cd/start.sh
+++ restore_cd/start.sh
@@ -6,7 +6,7 @@ export LC_CTYPE=en_EN.UTF-8
 export LANGUAGE=en_EN.UTF-8
 cd `dirname $0`
 echo "Starting restoration..."
-./urbackup_client --no-server --plugin ./libfsimageplugin.so --plugin ./libfileservplugin.so --plugin ./liburbackupclient.so --restore_mode true --logfile restore_mode.txt --loglevel debug > /dev/null 2>&1 &disown
+./urbackup_client --no-server --plugin ./libfsimageplugin.so --plugin ./libfileservplugin.so --plugin ./liburbackupclient.so --restore_mode true --logfile restore_mode.txt --loglevel debug > /dev/null 2>&1 &
 sleep 1
 ./urbackup_client --no-server --plugin ./liburbackupclient.so --restore_wizard true --logfile restore_wizard.txt --loglevel debug
 echo "Wizard stoped with error code: $?"
