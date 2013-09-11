3c9fdb7b1c6c1d7cf4d05245b436fe8862c9a7e9
Martin Raiber <martin@urbackup.org>
2013-09-11 21:41:18 +0200

    Wait after freezing list of open files being written to

--- urbackupclient/client.cpp
+++ urbackupclient/client.cpp
@@ -575,6 +575,8 @@ void IndexThread::indexDirs(void)
 	updateDirs();
 #ifdef _WIN32
 	//Invalidate cache
+	DirectoryWatcherThread::freeze();
+	Server->wait(10000);
 	DirectoryWatcherThread::update_and_wait();
 	changed_dirs=cd->getChangedDirs();
 	cd->moveChangedFiles();
@@ -586,7 +588,6 @@ void IndexThread::indexDirs(void)
 	{
 		cd->removeDeletedDir(deldirs[i]);
 	}
-	DirectoryWatcherThread::freeze();
 #endif
 	std::sort(changed_dirs.begin(), changed_dirs.end());
 
