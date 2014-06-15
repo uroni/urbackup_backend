1fc820978f33e6ae3c4424485d1119dc2bf927cc
Martin <martin@urbackup.org>
2014-06-15 17:55:11 +0200

    Reset before building

--- build_server.sh
+++ build_server.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+git reset --hard
+python3.3 build/replace_versions.py
+
 if ! test -e build_server_ok
 then
 	./switch_build.sh server
@@ -12,7 +15,8 @@ then
 	./configure
 	touch build_server_ok
 fi
+
 ./switch_build.sh server
-python3.3 build/replace_versions.py
+
 make
 make dist
