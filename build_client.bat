2434294f1be31bd556b6ad4c7a306f95b0c7af04
Martin <martin@urbackup.org>
2014-07-03 23:11:15 +0200

    Fixup file names for unfortunately renamed urbackupclient.dll => urbackup.dll

--- build_client.bat
+++ build_client.bat
@@ -19,6 +19,16 @@ git rev-parse HEAD >> "build_revision.bat"
 call build_revision.bat
 
 cd "%~dp0"
+
+copy /Y "Release Server 2003\urbackupclient_server03.dll" "Release Server 2003\urbackup_server03.dll"
+copy /Y "x64\Release Server 2003\urbackupclient_server03.dll" "x64\Release Server 2003\urbackup_server03.dll"
+
+copy /Y "Release\urbackupclient.dll" "Release\urbackup.dll"
+copy /Y "x64\Release\urbackupclient.dll" "x64\Release\urbackup.dll"
+
+copy /Y "Release WinXP\urbackupclient_xp.dll" "Release Server 2003\urbackup_xp.dll"
+
+
 FOR /F "tokens=*" %%G IN (pdb_dirs_client.txt) DO symstore add /compress /r /f "%~dp0%%G" /s "C:\symstore" /t "UrBackup Client /v "%build_revision%" /c "Release"
 
 :skip_symbols
