28f12ec1cd21a8729c228b5a2a16a66c8a61fad5
Martin <martin@urbackup.org>
2014-11-05 23:23:15 +0100

    Exit if no header was found and file was patched (cherry picked from commit 41b4771f4185484a8516f47bbdd558f86b1b6259)

--- urbackupserver/ChunkPatcher.cpp
+++ urbackupserver/ChunkPatcher.cpp
@@ -38,11 +38,11 @@ bool ChunkPatcher::ApplyPatch(IFile *file, IFile *patch)
 		if(has_header && next_header.patch_off==-1)
 		{
 			has_header=readNextValidPatch(patch, patchf_pos, &next_header);
+		}
 
-			if(!has_header && file_pos>=filesize)
-			{
-				break;
-			}
+		if(!has_header && file_pos>=filesize)
+		{
+			break;
 		}
 
 		unsigned int tr=buffer_size;
