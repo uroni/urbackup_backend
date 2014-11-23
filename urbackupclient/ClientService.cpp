27ccbf8fd473d9ec966aedbc2e09232f2a516924
Martin <martin@urbackup.org>
2014-11-23 18:31:49 +0100

    Correctly backup root directory on Linux

--- urbackupclient/ClientService.cpp
+++ urbackupclient/ClientService.cpp
@@ -1030,7 +1030,19 @@ bool ClientConnector::saveBackupDirs(str_map &args, bool server_default)
 			name=removeChars(name);
 
 			if(dir[dir.size()-1]=='\\' || dir[dir.size()-1]=='/' )
+			{
 				dir.erase(dir.size()-1,1);
+#ifndef _WIN32
+				if(dir.empty())
+				{
+					dir=L"/";
+					if(name.empty())
+					{
+						name=L"root";
+					}
+				}
+#endif
+			}
 
 			q2->Bind(name);
 			if(q2->Read().empty()==false)
--- urbackupclient/client.cpp
+++ urbackupclient/client.cpp
@@ -216,6 +216,10 @@ std::wstring add_trailing_slash(const std::wstring &strDirName)
 	{
 		return strDirName+os_file_sep();
 	}
+	else
+	{
+		return os_file_sep();
+	}
 	return strDirName;
 }
 
@@ -915,6 +919,12 @@ void IndexThread::indexDirs(void)
 			//db->Write("BEGIN IMMEDIATE;");
 			last_transaction_start=Server->getTimeMS();
 			index_root_path=mod_path;
+#ifndef _WIN32
+			if(index_root_path.empty())
+			{
+				index_root_path=os_file_sep();
+			}
+#endif
 			initialCheck( backup_dirs[i].path, mod_path, backup_dirs[i].tname, outfile, true,
 				backup_dirs[i].optional, !(patterns_changed || tokens_changed) );
 
