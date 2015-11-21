ee28d02fe5ab37e16daaac7a74bbf74cac05832d
Martin Raiber <martin@urbackup.org>
2015-11-21 17:36:34 +0100

    New app to copy a file skipping identical blocks via hashes

--- urbackupcommon/chunk_hasher.h
+++ urbackupcommon/chunk_hasher.h
@@ -9,7 +9,9 @@ public:
 	virtual bool handle_not_enough_space(const std::wstring &path)=0;
 };
 
-std::string build_chunk_hashs(IFile *f, IFile *hashoutput, INotEnoughSpaceCallback *cb, bool ret_sha2, IFile *copy, bool modify_inplace, int64* inplace_written=NULL);
+std::string build_chunk_hashs(IFile *f, IFile *hashoutput, INotEnoughSpaceCallback *cb,
+	bool ret_sha2, IFile *copy, bool modify_inplace, int64* inplace_written=NULL,
+	IFile* hashinput=NULL);
 
 bool writeRepeatFreeSpace(IFile *f, const char *buf, size_t bsize, INotEnoughSpaceCallback *cb);
 
