091e31ec5f5ac694256bd05a75d8c667f7388da0
Martin <martin@urbackup.org>
2016-01-04 19:25:10 +0100

    Disable shadow copy excluding

--- fsimageplugin/FSImageFactory.cpp
+++ fsimageplugin/FSImageFactory.cpp
@@ -150,10 +150,10 @@ IFilesystem *FSImageFactory::createFilesystem(const std::string &pDev, bool read
 		}
 
 		//Exclude shadow storage files
-		if(pDev.find("HarddiskVolumeShadowCopy") != std::string::npos)
+		/*if(pDev.find("HarddiskVolumeShadowCopy") != std::string::npos)
 		{
 			fs->excludeFiles(pDev + "\\System Volume Information", "{3808876b-c176-4e48-b7ae-04046e6cc752}");
-		}
+		}*/
 #endif
 		
 		/*
