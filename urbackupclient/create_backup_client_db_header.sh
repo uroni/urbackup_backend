326d462c629a14801d9620f340344e81d65e6f9d
Martin Raiber <martin@urbackup.org>
2016-01-05 17:01:17 +0100

    Regenerated db headers

--- urbackupclient/create_backup_client_db_header.sh
+++ urbackupclient/create_backup_client_db_header.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 
-cat backup_client.db | python3 -c "import zlib,sys; print(zlib.compress(sys.stdin.buffer.read()))" > backup_client_db.z
+cat backup_client.db | python3 -c "import zlib,sys; sys.stdout.buffer.write(zlib.compress(sys.stdin.buffer.read()))" > backup_client_db.z
 xxd -i backup_client_db.z > backup_client_db.h
--- urbackupserver/create_backup_server_db_header.sh
+++ urbackupserver/create_backup_server_db_header.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 
-cat backup_server.db | python3 -c "import zlib,sys; print(zlib.compress(sys.stdin.buffer.read()))" > backup_server_db.z
+cat backup_server.db | python3 -c "import zlib,sys; sys.stdout.buffer.write(zlib.compress(sys.stdin.buffer.read()))" > backup_server_db.z
 xxd -i backup_server_db.z > backup_server_db.h
