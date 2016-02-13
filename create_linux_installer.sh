4f2eb3e99bcb7b0c6fbd8eecdb017d6102820063
Martin <martin@urbackup.org>
2016-02-13 13:06:41 +0100

    Create second installer with debug symbols

--- create_linux_installer.sh
+++ create_linux_installer.sh
@@ -15,19 +15,8 @@ autoreconf --install
 rm -R install-data || true
 mkdir -p install-data
 
-for arch in i386-linux-eng x86_64-linux-eng armv6-linux-engeabihf aarch64-linux-eng
-do
-	echo "Compiling for architecture $arch..."
-    ./configure --enable-headless --enable-clientupdate CFLAGS="-target $arch" CPPFLAGS="-target $arch -DURB_THREAD_STACKSIZE64=8388608 -DURB_THREAD_STACKSIZE32=1048576 -ffunction-sections -fdata-sections" LDFLAGS="-target $arch -Wl,--gc-sections" CXX="ecc++" CC="ecc" CXXFLAGS="-ggdb -Os" --with-crypto-prefix=/usr/local/ellcc/libecc
-    make clean
-    make -j4
-    rm -R install-data/$arch || true
-    mkdir -p install-data/$arch
-    cp urbackupclientbackend install-data/$arch/
-	ecc-strip install-data/$arch/urbackupclientbackend
-	cp urbackupclientctl install-data/$arch/
-	ecc-strip install-data/$arch/urbackupclientctl
-done
+rm -R install-data-dbg || true
+mkdir -p install-data-dbg
 
 mkdir -p install-data/backup_scripts
 cp urbackupclient/backup_scripts/* install-data/backup_scripts/
@@ -42,6 +31,26 @@ cp defaults_client install-data/
 cp linux_snapshot/* install-data/
 chmod +x install-data/*_filesystem_snapshot
 
+cp -R install-data/* install-data-dbg/
+
+for arch in i386-linux-eng x86_64-linux-eng armv6-linux-engeabihf aarch64-linux-eng
+do
+	echo "Compiling for architecture $arch..."
+    ./configure --enable-headless --enable-clientupdate CFLAGS="-target $arch" CPPFLAGS="-target $arch -DURB_THREAD_STACKSIZE64=8388608 -DURB_THREAD_STACKSIZE32=1048576 -ffunction-sections -fdata-sections" LDFLAGS="-target $arch -Wl,--gc-sections" CXX="ecc++" CC="ecc" CXXFLAGS="-ggdb -Os" --with-crypto-prefix=/usr/local/ellcc/libecc
+    make clean
+    make -j4
+    rm -R install-data/$arch || true
+    mkdir -p install-data/$arch
+	rm -R install-data-dbg/$arch || true
+    mkdir -p install-data-dbg/$arch
+    cp urbackupclientbackend install-data/$arch/
+	cp urbackupclientbackend install-data-dbg/$arch/
+	ecc-strip install-data/$arch/urbackupclientbackend
+	cp urbackupclientctl install-data/$arch/
+	cp urbackupclientctl install-data-dbg/$arch/
+	ecc-strip install-data/$arch/urbackupclientctl
+done
+
 rm -R linux-installer || true
 mkdir -p linux-installer
 
@@ -51,4 +60,14 @@ cd ..
 
 cp install_client_linux.sh linux-installer/
 
+rm -R linux-installer-dbg || true
+mkdir -p linux-installer-dbg
+
+cd install-data-dbg
+tar czf ../linux-installer-dbg/install-data.tar.gz *
+cd ..
+
+cp install_client_linux.sh linux-installer-dbg/
+
 makeself --nocomp --nomd5 --nocrc linux-installer "UrBackupUpdateLinux.sh" "UrBackup Client Installer for Linux" ./install_client_linux.sh
+makeself --nocomp --nomd5 --nocrc linux-installer-dbg "UrBackupUpdateLinux-dbg.sh" "UrBackup Client Installer for Linux (debug)" ./install_client_linux.sh
