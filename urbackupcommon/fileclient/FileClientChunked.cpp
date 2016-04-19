ddb4def1519ee2781e01a537c80308e840d484ee
Martin <martin@urbackup.org>
2016-04-19 20:08:01 +0200

    Do not switch state for zero length chunk
    (cherry picked from commit f565154d5691d5422269cc34d0a52a0d2c813068)

--- urbackupcommon/fileclient/FileClientChunked.cpp
+++ urbackupcommon/fileclient/FileClientChunked.cpp
@@ -961,7 +961,10 @@ void FileClientChunked::State_Acc(bool ignore_filesize, IFile** sparse_extents_f
 						+big_hash_size+chunknum*small_hash_size);
 				}				
 
-				state=CS_CHUNK;
+				if (adler_remaining > 0)
+				{
+					state = CS_CHUNK;
+				}
 				adler_hash=urb_adler32(0, NULL, 0);
 
 			}break;
