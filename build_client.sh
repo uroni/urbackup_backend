cd0659d785a8eac6744c60eef92bfc596a1d8ef1
Martin <martin@urbackup.org>
2016-06-11 16:06:29 +0200

    Start embedding crypto++

--- build_client.sh
+++ build_client.sh
@@ -8,11 +8,15 @@ python3 build/replace_versions.py
 if ! test -e build_client_ok
 then
 	./switch_build.sh client
+	wget https://www.cryptopp.com/cryptopp563.zip -O cryptoplugin/cryptopp563.zip
+	cd cryptoplugin
+	unzip cryptopp563.zip
+	cd ..
 	autoreconf || true
 	automake --add-missing || true
 	libtoolize || true
 	autoreconf --install
-	./configure
+	./configure --enable-embedded-cryptopp
 	touch build_client_ok
 fi
 ./switch_build.sh client
