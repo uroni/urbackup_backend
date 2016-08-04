48e73c3bc712fa6c0c93af9d3a14e49e72b60ed8
Martin <martin@urbackup.org>
2016-08-04 19:13:27 +0200

    Remove Windows XP/Server 2003 remains

--- build_client_backend.bat
+++ build_client_backend.bat
@@ -17,22 +17,4 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild UrBackupBackend.sln /p:Configuration="Release x64" /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild urbackupclient\urbackupclient.vcxproj /p:Configuration="Release Server 2003" /p:Platform="win32"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-mkdir "Release Server 2003"
-copy /Y "urbackupclient\Release Server 2003\*" "Release Server 2003\"
-
-msbuild urbackupclient\urbackupclient.vcxproj /p:Configuration="Release Server 2003" /p:Platform="x64"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-mkdir "x64\Release Server 2003"
-copy /Y "urbackupclient\x64\Release Server 2003\*" "x64\Release Server 2003\"
-
-msbuild urbackupclient\urbackupclient.vcxproj /p:Configuration="Release WinXP" /p:Platform="win32"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-mkdir "Release WinXP"
-copy /Y "urbackupclient\Release WinXP\*" "Release WinXP\"
-
 exit /b 0
