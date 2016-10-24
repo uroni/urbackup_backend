2af877b807a4a11f63ec7981d47944052d8f401f
Martin <martin@urbackup.org>
2016-10-24 17:55:53 +0200

    Fix script

--- download_cryptopp.sh
+++ download_cryptopp.sh
@@ -6,7 +6,7 @@ CRYPTOPP_NAME="cryptopp565.zip"
 EXPECTED_SHA256="c67c64693f32195e69d3d7e5bdf47afbd91e8b69d0407a2bc68a745d9dbebb26"
 
 cd cryptoplugin
-wget http://buildserver.urbackup.org/$CRYPTOPP_NAME -O cryptoplugin/$CRYPTOPP_NAME
+wget http://buildserver.urbackup.org/$CRYPTOPP_NAME -O $CRYPTOPP_NAME
 SHASUM=`sha256sum $CRYPTOPP_NAME | cut -d" " -f1`
 if [ $SHASUM != $EXPECTED_SHA256 ]
 then
