0816eab977a5d576ddb5bf4e5fdddd94892ec04a
Martin <martin@urbackup.org>
2017-02-24 20:29:46 +0100

    Fix update url construction
    (cherry picked from commit 73cc92b9db23b07bd023f2980ed96a1437959e9c)

--- urbackupserver/server_update.cpp
+++ urbackupserver/server_update.cpp
@@ -256,6 +256,12 @@ void ServerUpdate::read_update_location()
 	if (!read_update_location.empty())
 	{
 		urbackup_update_url_alt = read_update_location;
-		urbackup_update_url = urbackup_update_url_alt + "/2.1.x";
+		urbackup_update_url = urbackup_update_url_alt;
+
+		if (!urbackup_update_url.empty()
+			&& urbackup_update_url[urbackup_update_url.size() - 1] != '/')
+			urbackup_update_url += "/";
+
+		urbackup_update_url += "2.1.x/";
 	}
 }
