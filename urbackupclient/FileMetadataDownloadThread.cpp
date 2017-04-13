a4ec1e420de9ac218be75474df76e574e7e0872f
Martin <martin@urbackup.org>
2017-04-13 02:07:56 +0200

    Fix client compile on FreeBSD
    (cherry picked from commit c0586ac45d9bd92fc054da2d0476359d37f83fd1)

--- urbackupclient/FileMetadataDownloadThread.cpp
+++ urbackupclient/FileMetadataDownloadThread.cpp
@@ -569,7 +569,7 @@ namespace
     {
 #ifndef __linux__
 
-#if defined(__APPLE__) || defined(__FreeBSD__)
+#if defined(__APPLE__)
 		int64 chflags_mask = UF_NODUMP | UF_IMMUTABLE | UF_APPEND | UF_OPAQUE | UF_HIDDEN | SF_ARCHIVED | SF_IMMUTABLE | SF_APPEND;
 #else
 		int64 chflags_mask = SF_APPEND | SF_ARCHIVED | SF_IMMUTABLE | SF_NOUNLINK | UF_APPEND | UF_ARCHIVE | UF_HIDDEN | UF_IMMUTABLE | UF_NODUMP | UF_NOUNLINK | UF_OFFLINE | UF_OPAQUE | UF_READONLY | UF_REPARSE | UF_SPARSE | UF_SYSTEM;
