1e67616cc36ea43149b04661436c7209dd0a02bb
Martin <martin@urbackup.org>
2017-07-28 18:43:31 +0200

    Improve hostname regex

--- urbackupserver/www/js/urbackup.js
+++ urbackupserver/www/js/urbackup.js
@@ -3733,7 +3733,7 @@ function getInternetSettings()
 {	
 	if(!I('internet_server_port')) return "";
 	if(!validate_text_int(["internet_server_port"]) ) return null;
-	if(!validate_text_regex([{ id: "internet_server", regexp: /(^[\w-]+(\.[\w-]*)*$)|(^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$)|(^$)/i }])) return null;
+	if(!validate_text_regex([{ id: "internet_server", regexp: /(((;|^)(([\w-]+(\.[\w-]*)*)|((?!0)(?!.*\.)((1?\d?\d|25[0-5]|2[0-4]\d)(\.)){4})))+$)|(^$)/i }])) return null;
 	var pars="";
 	for(var i=0;i<g.internet_settings_list.length;++i)
 	{
