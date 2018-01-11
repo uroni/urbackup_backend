29eccebac50ba3df1f03ef6671d86475964a7fb5
Martin <martin@urbackup.org>
2018-01-11 19:06:28 +0100

    Fix inode_db cleanup

--- urbackupserver/server_cleanup.cpp
+++ urbackupserver/server_cleanup.cpp
@@ -184,7 +184,7 @@ void ServerCleanupThread::operator()(void)
 	else
 	{
 		ServerSettings settings(db);
-		if (os_directory_exists(settings.getSettings()->backupfolder + os_file_sep() + "inode_db"));
+		if (os_directory_exists(settings.getSettings()->backupfolder + os_file_sep() + "inode_db"))
 		{
 			Server->deleteFile(settings.getSettings()->backupfolder + os_file_sep() + "inode_db" + os_file_sep() + "inode_db.lmdb");
 			Server->deleteFile(settings.getSettings()->backupfolder + os_file_sep() + "inode_db" + os_file_sep() + "inode_db.lmdb-lock");
