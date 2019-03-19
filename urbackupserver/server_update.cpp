9f4c73f7f4594b45d136b06645e3870a0c7275a1
Martin <martin@urbackup.org>
2019-03-19 22:23:43 +0100

    Use 2.3.x cbt client

--- urbackupserver/server_update.cpp
+++ urbackupserver/server_update.cpp
@@ -300,6 +300,6 @@ void ServerUpdate::read_update_location()
 			&& urbackup_update_url[urbackup_update_url.size() - 1] != '/')
 			urbackup_update_url += "/";
 
-		urbackup_update_url += "2.2.x/";
+		urbackup_update_url += "2.3.x/";
 	}
 }
