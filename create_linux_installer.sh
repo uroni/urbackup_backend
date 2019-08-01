d2aad9edfe837f7be4aaff50e900141a62633df5
Martin <martin@urbackup.org>
2019-08-01 19:09:48 +0200

    Replace adhoc file with empty file

--- create_linux_installer.sh
+++ create_linux_installer.sh
@@ -77,6 +77,7 @@ export STRIP=$TOOLCHAIN/bin/$TARGET-strip
 #sed -i "s@$CRYPTOPP_LDFLAGS@$CRYPTOPP_LDFLAG@g" Makefile.am
 sed -i 's@adhoc.cpp@empty.cpp@' cryptoplugin/src/Makefile.am
 touch cryptoplugin/src/empty.cpp
+touch cryptoplugin/src/empty.cpp.proto
 ./configure --enable-headless --enable-c-ares --enable-embedded-cryptopp LDFLAGS="-static -Wl,--gc-sections -O2 $NDK_CPUFLAGS -flto" --host $TARGET --with-zlib=$TOOLCHAIN/sysroot/usr --with-crypto-prefix=$TOOLCHAIN/sysroot/usr CPPFLAGS="-DURB_THREAD_STACKSIZE64=8388608 -DURB_THREAD_STACKSIZE32=1048576 -DURB_WITH_CLIENTUPDATE -ffunction-sections -fdata-sections -ggdb -O2 -flto" CFLAGS="-ggdb -O2 -flto $NDK_CPUFLAGS" CXXFLAGS="-ggdb -O2 -flto $NDK_CPUFLAGS -I$NDK/sources/android/cpufeatures/"
 }
 
