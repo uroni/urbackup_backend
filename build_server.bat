9c9db8c47bf76c92983e1394a5df49c2c871bf3a
Martin <martin@urbackup.org>
2020-01-29 16:19:06 +0100

    Upgrade to Visual Studio 2019

--- build_server.bat
+++ build_server.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 
 call update_deps.bat
 
