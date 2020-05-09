09caabff1b8048739668d3400a83be91e55ec3ad
Martin <martin@urbackup.org>
2020-05-09 14:18:47 +0200

    Disable LMDB writemap
    (cherry picked from commit 0672e0855b7262cf24ccdfa819b845175bd93cad)
    
    # Conflicts:
    #	clouddrive/SingleFileStorage.cpp

--- urbackupserver/LMDBFileIndex.cpp
+++ urbackupserver/LMDBFileIndex.cpp
@@ -529,7 +529,7 @@ bool LMDBFileIndex::create_env()
 
 		os_create_dir("urbackup/fileindex");
 
-		unsigned int flags = MDB_NOSUBDIR|MDB_WRITEMAP;
+		unsigned int flags = MDB_NOSUBDIR;
 		if(no_sync)
 		{
 			flags|=MDB_NOSYNC;
