2004dbfbe6f6e665137d756d6c96c4ff75c84f10
Martin Raiber <urpc@gmx.de>
2020-08-06 12:36:52 +0200

    Merge pull request #34 from Moisie2000/fix_macos_uninstaller
    Fix macOS uninstaller
    
    (cherry picked from commit d14d792c7d8c1308e0e31f883e97f9192939e70f)

--- osx_installer/uninstall1.sh
+++ osx_installer/uninstall1.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 
 function finish {
-	if [[ $OK == "true" ]]
+	if [[ $OK == true ]]
 	then
 		osascript -e 'display notification "UrBackup was uninstalled successfully." with title "UrBackup"'
+		killall urbackupclientgui || true
 	else
 		osascript -e 'display notification "Failed to uninstall UrBackup" with title "UrBackup"'
 	fi
@@ -18,8 +19,6 @@ if /bin/launchctl list "org.urbackup.client.backend" &> /dev/null; then
 	/bin/launchctl stop "org.urbackup.client.backend" 
     /bin/launchctl unload "/Library/LaunchDaemons/org.urbackup.client.plist"
 fi
-
-killall urbackupclientgui || true
 
 /Applications/UrBackup\ Client.app/Contents/MacOS/urbackupclientgui remove_login_item
 
