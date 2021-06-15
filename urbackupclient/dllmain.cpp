7812453543f1b8d4e89fb61187e2423604e1f8f2
Martin Raiber <martin@urbackup.org>
2021-06-15 16:47:26 +0200

    Start filesrv before accepting commands

--- urbackupclient/dllmain.cpp
+++ urbackupclient/dllmain.cpp
@@ -544,6 +544,19 @@ DLLEXPORT void LoadActions(IServer* pServer)
 	bool do_leak_check=(Server->getServerParameter("leak_check")=="true");
 
 	ClientConnector::init_mutex();
+
+	filesrv_pluginid = Server->StartPlugin("fileserv", params);
+
+	IndexThread* it = new IndexThread();
+	if (!do_leak_check)
+	{
+		Server->createThread(it, "file indexing");
+	}
+	else
+	{
+		indexthread_ticket = Server->getThreadPool()->execute(it, "file indexing");
+	}
+
 	unsigned short urbackup_serviceport = default_urbackup_serviceport;
 	if(!Server->getServerParameter("urbackup_serviceport").empty())
 	{
@@ -557,18 +570,6 @@ DLLEXPORT void LoadActions(IServer* pServer)
 	}
 
 	Server->StartCustomStreamService(new ClientService(), "urbackupserver", urbackup_serviceport, -1, serviceport_bind_target);
-
-	filesrv_pluginid=Server->StartPlugin("fileserv", params);
-
-	IndexThread *it=new IndexThread();
-	if(!do_leak_check)
-	{
-		Server->createThread(it, "file indexing");
-	}
-	else
-	{
-		indexthread_ticket=Server->getThreadPool()->execute(it, "file indexing");
-	}
 
 	internetclient_tickets=InternetClient::start(do_leak_check);
 
