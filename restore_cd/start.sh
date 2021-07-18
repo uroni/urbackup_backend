321202ad9f0b2c2f1581a36565fbb7ff7d9845b4
Martin Raiber <martin@urbackup.org>
2021-07-18 13:34:53 +0200

    Ask for restore UI

--- restore_cd/start.sh
+++ restore_cd/start.sh
@@ -25,9 +25,20 @@ chmod +x /usr/share/console-setup/keyboard-configuration.config
 dpkg-reconfigure keyboard-configuration
 service keyboard-setup restart
 setupcon
-echo "Starting restore wizard..."
-./urbackuprestoreclient --restore-wizard --logfile restore_wizard.txt --loglevel debug
-echo "Wizard stoped with error code: $?"
-tail -n 100 restore_wizard.txt
-read -p "Press any key to continue..."
+while ! dialog --menu "Please select restore user interface" 15 50 10 "g" "Graphical/modern interface (recommended)" "t" "Text/console-based interface" 2> out
+do
+    echo "Please select user interface"
+    read -p "Press any key to continue..."
+done
+SELUI=$(cat out)
+if [ "x$SELUI" = xt ]
+then
+    echo "Starting restore wizard..."
+    ./urbackuprestoreclient --restore-wizard --logfile restore_wizard.txt --loglevel debug
+    echo "Wizard stoped with error code: $?"
+    tail -n 100 restore_wizard.txt
+    read -p "Press any key to continue..."
+else
+    systemctl start graphical.target
+fi
 
