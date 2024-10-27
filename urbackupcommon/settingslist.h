aee715b95677f3a5e15498a5d991615fd07d740b
Martin Raiber <martin@urbackup.org>
2024-10-27 20:19:34 +0100

    Fix build

--- urbackupcommon/settingslist.h
+++ urbackupcommon/settingslist.h
@@ -2,7 +2,7 @@
 #include <vector>
 
 std::vector<std::string> getSettingsList(void);
-std::vector<std::string> getClientConfigurableSettingsList();
+std::vector<std::string> getClientConfigurableSettingsList(const bool with_max_backups_config);
 std::vector<std::string> getClientMergableSettingsList();
 std::vector<std::string> getOnlyServerClientSettingsList(void);
 std::vector<std::string> getGlobalizedSettingsList(void);
