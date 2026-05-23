0bacde15a1bc925f7896653184c9a2da275ed41c
Martin Raiber <martin@urbackup.org>
2026-05-23 23:31:17 +0200

    Fix setting vcpkg triplet

--- build_client_backend.bat
+++ build_client_backend.bat
@@ -1,21 +1,21 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 
-msbuild UrBackupBackend.sln /p:Configuration=Release /p:Platform="win32"  /p:vcpkgTriplet="x86-windows-static-md"
+msbuild UrBackupBackend.sln /p:Configuration=Release /p:Platform="win32"  /p:vcpkgTriplet="x86-windows-static-md" /p:VcpkgCurrentTriplet="x86-windows-static-md"
 if %errorlevel% neq 0 exit /b %errorlevel% 
 
-msbuild UrBackupBackend.sln /p:Configuration=Release /p:Platform="x64"  /p:vcpkgTriplet="x64-windows-static-md"
+msbuild UrBackupBackend.sln /p:Configuration=Release /p:Platform="x64"  /p:vcpkgTriplet="x64-windows-static-md" /p:VcpkgCurrentTriplet="x64-windows-static-md"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild UrBackupBackend.sln /p:Configuration=Release /p:Platform="ARM64"  /p:vcpkgTriplet="arm64-windows-static-md"
+msbuild UrBackupBackend.sln /p:Configuration=Release /p:Platform="ARM64"  /p:vcpkgTriplet="arm64-windows-static-md" /p:VcpkgCurrentTriplet="arm64-windows-static-md"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild CompiledServer.vcxproj /p:Configuration="Release Service" /p:Platform="x64"  /p:vcpkgTriplet="x64-windows-static-md"
+msbuild CompiledServer.vcxproj /p:Configuration="Release Service" /p:Platform="x64"  /p:vcpkgTriplet="x64-windows-static-md"  /p:VcpkgCurrentTriplet="x64-windows-static-md"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild CompiledServer.vcxproj /p:Configuration="Release Service" /p:Platform="win32"  /p:vcpkgTriplet="x86-windows-static-md"
+msbuild CompiledServer.vcxproj /p:Configuration="Release Service" /p:Platform="win32"  /p:vcpkgTriplet="x86-windows-static-md" /p:VcpkgCurrentTriplet="x86-windows-static-md"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild CompiledServer.vcxproj /p:Configuration="Release Service" /p:Platform="arm64"  /p:vcpkgTriplet="arm64-windows-static-md"
+msbuild CompiledServer.vcxproj /p:Configuration="Release Service" /p:Platform="arm64"  /p:vcpkgTriplet="arm64-windows-static-md" /p:VcpkgCurrentTriplet="arm64-windows-static-md"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b 0
